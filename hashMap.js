10d61817d3133f035347655a31189e20569d2f2c
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-10 14:49:24 +0300

    Add hash method to generate hash code by key

--- hashMap.js
+++ hashMap.js
@@ -8,4 +8,19 @@ export class HashMap {
     this.#loadFactor = 0.75;
     this.#buckets = Array.from({ size: this.#capacity }, () => []);
   }
+
+  hash(key) {
+    const normalizedKey =
+      typeof key === "object" ? JSON.stringify(key) : String(key);
+
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < normalizedKey.length; i++) {
+      hashCode =
+        (primeNumber * hashCode + normalizedKey.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
--- hashMap.test.js
+++ hashMap.test.js
@@ -1,3 +1,33 @@
 import { HashMap } from "./hashMap";
 
-describe("Testing HashMap behaviour", () => {});
+describe("Testing HashMap behaviour", () => {
+  test("hash function produces a hash code 11 from given key Tyrion", () => {
+    const hashMap = new HashMap();
+    expect(hashMap.hash("Tyrion")).toBe(11);
+  });
+
+  test("hash function produces a hash code 10 from given key Jaime", () => {
+    const hashMap = new HashMap();
+    expect(hashMap.hash("Jaime")).toBe(10);
+  });
+
+  test("hash function produces a hash code 7 from given key Cersei", () => {
+    const hashMap = new HashMap();
+    expect(hashMap.hash("Cersei")).toBe(7);
+  });
+
+  test("hash function produces a hash code 0 from given key ''", () => {
+    const hashMap = new HashMap();
+    expect(hashMap.hash("")).toBe(0);
+  });
+
+  test("hash function produces a hash code for given non-string key ", () => {
+    const hashMap = new HashMap();
+    expect(hashMap.hash(-5)).toBe(8);
+    expect(hashMap.hash([1, 2, 3])).toBe(10);
+  });
+
+  test("hash function produces a hash code on anonymous HashMap instance", () => {
+    expect(new HashMap().hash("Cersei")).toBe(7);
+  });
+});
