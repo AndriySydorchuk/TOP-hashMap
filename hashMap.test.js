39b0b6c0a3ec4f5ed1845b4564722e7c4cf5d984
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-10 15:00:35 +0300

    Refactor test names

--- hashMap.test.js
+++ hashMap.test.js
@@ -2,33 +2,33 @@ import { HashMap } from "./hashMap";
 
 describe("Testing HashMap behaviour", () => {
   describe("hash function", () => {
-    test("hash function produces a hash code 11 from given key Tyrion", () => {
+    test("returns hash code 11 for key Tyrion", () => {
       const hashMap = new HashMap();
       expect(hashMap.hash("Tyrion")).toBe(11);
     });
 
-    test("hash function produces a hash code 10 from given key Jaime", () => {
+    test("returns hash code 10 for key Jaime", () => {
       const hashMap = new HashMap();
       expect(hashMap.hash("Jaime")).toBe(10);
     });
 
-    test("hash function produces a hash code 7 from given key Cersei", () => {
+    test("returns hash code 7 for key Cersei", () => {
       const hashMap = new HashMap();
       expect(hashMap.hash("Cersei")).toBe(7);
     });
 
-    test("hash function produces a hash code 0 from given key ''", () => {
+    test("returns hash code 0 for key ''", () => {
       const hashMap = new HashMap();
       expect(hashMap.hash("")).toBe(0);
     });
 
-    test("hash function produces a hash code for given non-string key ", () => {
+    test("returns hash code for non-string key ", () => {
       const hashMap = new HashMap();
       expect(hashMap.hash(-5)).toBe(8);
       expect(hashMap.hash([1, 2, 3])).toBe(10);
     });
 
-    test("hash function produces a hash code on anonymous HashMap instance", () => {
+    test("returns hash code on anonymous HashMap instance", () => {
       expect(new HashMap().hash("Cersei")).toBe(7);
     });
   });
